04c0a138408490e9141f17813b493e98cd371dcf
GennYoon <yoonwonyoul@webchemist.net>
2024-08-12 16:27:27 +0900

    feat: slack message 개인 맨션으로 변경

--- src/index.ts
+++ src/index.ts
@@ -5,47 +5,27 @@ import * as core from "@actions/core";
 import * as github from "@actions/github";
 import axios from "axios";
 
-const sendSlackMessage = async ({
-  slack_url,
-  reviewers,
-  owner,
-  title,
-  pr_url,
-  repo_name,
-}: any) => {
-  axios({
+const sendSlackMessage = async ({ channel, slack_url, owner, title, pr_url, repo_name }: any) => {
+  await axios({
     method: "POST",
     headers: { "Content-Type": "application/json" },
     url: slack_url,
     data: {
+      channel,
       text: "PR Review 요청입니다.",
       blocks: [
         {
           type: "section",
           text: {
             type: "mrkdwn",
-            text: `${owner}님이 \`${repo_name}\` PR을 요청했습니다.`,
+            text: `${owner}님이 \`${repo_name}\`에서 PR을 요청했습니다.`,
           },
         },
         {
-          type: "section",
+          type: "context",
           text: {
             type: "mrkdwn",
-            text: `*PR Title*: ${title}`,
-          },
-        },
-        {
-          type: "section",
-          text: {
-            type: "mrkdwn",
-            text: `*PR URL*: ${pr_url}`,
-          },
-        },
-        {
-          type: "section",
-          text: {
-            type: "mrkdwn",
-            text: `*Reviewers*: ${reviewers.join(", ")}`,
+            text: `*PR Title*: ${title}\n*PR URL*: ${pr_url}`,
           },
         },
       ],
@@ -55,17 +35,20 @@ const sendSlackMessage = async ({
 
 (async () => {
   try {
-    const token = core.getInput("token");
+    const _channels = core.getInput("slack-channels") ?? {};
+    let slack_channels: Record<string, string> = {};
+    // const token = core.getInput("token");
     const slack_url = core.getInput("slack-url");
     const { payload } = github.context;
     const { pull_request, sender, repository } = payload;
 
-    const {
-      title,
-      labels,
-      html_url: pr_url,
-      requested_reviewers,
-    } = pull_request!;
+    try {
+      slack_channels = JSON.parse(_channels);
+    } catch {
+      slack_channels = {};
+    }
+
+    const { title, labels, html_url: pr_url, requested_reviewers } = pull_request!;
     const repo_name = repository?.full_name;
     if (requested_reviewers.length === 0) {
       return;
@@ -73,15 +56,18 @@ const sendSlackMessage = async ({
 
     const owner = sender?.login;
     const reviewers = requested_reviewers.map(({ login }: any) => login);
+    const channels = reviewers.map((reviewer: string) => slack_channels[reviewer]);
 
-    await sendSlackMessage({
-      slack_url,
-      reviewers,
-      owner,
-      title,
-      pr_url,
-      repo_name,
-    });
+    for await (const channel of channels) {
+      await sendSlackMessage({
+        slack_url,
+        channel,
+        owner,
+        title,
+        pr_url,
+        repo_name,
+      });
+    }
   } catch (error: any) {
     core.setFailed(error.message);
   }
